d7fe2432b9af5919d4ad328f0eabaa9a20c80bba
Damian Klekot <damian.m.klekot@gmail.com>
2022-04-26 14:27:54 +0100

    fixed value on press

--- src/Components/CalculatorComponents/Numbers.js
+++ src/Components/CalculatorComponents/Numbers.js
@@ -1,32 +1,34 @@
 import './numbers.css';
 import React, { useState } from 'react';
+import { toHaveDisplayValue } from '@testing-library/jest-dom/dist/matchers';
 
 
 const Numbers = () =>{
 
-    let [numberValue, setNumberValue] = useState(0);
+    let [numberValue, setNumberValue] = useState();
 
-    function handleNumberValue(event){
-        setNumberValue(event.target.value);
-        console.log(numberValue);
+    const clickHander = (e) =>{
+        setNumberValue(e.target.value);
+        console.log(e.target.value);
     }
+
 
     return(
         <div className='numbersPosition'>
-            <button type='button' className='n9'id='9' value={9} onClick={handleNumberValue} > 9 </button>
-            <button type='button' className='n8'id='8'value={8} onClick={handleNumberValue} > 8 </button>
-            <button type='button' className='n7'id='7'value={7} onClick={handleNumberValue}  > 7 </button>
+            <button type='button' className='n9'id='9' value={9} onClick={e => clickHander(e, "value")}> 9 </button>
+            <button type='button' className='n8'id='8' value={8} onClick={e => clickHander(e, "value")}> 8 </button>
+            <button type='button' className='n7'id='7' value={7} onClick={e => clickHander(e, "value")}> 7 </button>
 
-            <button type='button' className='n6'id='6' value={6} onClick={handleNumberValue}> 6 </button>
-            <button type='button' className='n5'id='5' value={5} onClick={handleNumberValue}> 5 </button>
-            <button type='button' className='n4'id='4' value={4} onClick={handleNumberValue}> 4 </button>
+            <button type='button' className='n6'id='6' value={6} onClick={e => clickHander(e, "value")}> 6 </button>
+            <button type='button' className='n5'id='5' value={5} onClick={e => clickHander(e, "value")}> 5 </button>
+            <button type='button' className='n4'id='4' value={4} onClick={e => clickHander(e, "value")}> 4 </button>
 
-            <button type='button' className='n3'id='3' value={3} onClick={handleNumberValue}> 3 </button>
-            <button type='button' className='n2'id='2' value={2} onClick={handleNumberValue}> 2 </button>
-            <button type='button' className='n1'id='1' value={1} onClick={handleNumberValue}> 1 </button>
+            <button type='button' className='n3'id='3' value={3} onClick={e => clickHander(e, "value")}> 3 </button>
+            <button type='button' className='n2'id='2' value={2} onClick={e => clickHander(e, "value")}> 2 </button>
+            <button type='button' className='n1'id='1' value={1} onClick={e => clickHander(e, "value")}> 1 </button>
 
-            <button type='button' className='n0'id='0' value={0} onClick={handleNumberValue}> 0 </button>
-            <button type='button' className='nDot'id='Dot' value={'.'} onClick={handleNumberValue } > . </button>
+            <button type='button' className='n0'id='0' value={0} onClick={e => clickHander(e, "value")}> 0 </button>
+            <button type='button' className='nDot'id='Dot' value={'.'} onClick={e => clickHander(e, "value")}> . </button>
         </div>
     );
 }
